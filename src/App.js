8adbc44ad70d32ed19a47d3bbb231bb7a4b5639c
mateo <mateo.b1496@gmail.com>
2022-11-10 15:20:18 -0300

    cambios app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import "./estilos.css";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './components/Cart';
+import CartProvider from './components/CartContext';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+           <BrowserRouter>
+              <NavBar />
+                 <Routes>
+                     <Route path="/" element={<ItemListContainer titulo="Los mejores estilos para ellos" parrafo="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, deserunt quisquam. Ut tempore vero, cupiditate dolorum enim repudiandae, illo voluptatibus nihil ab, obcaecati ipsa amet? Commodi tenetur aperiam modi dolorum." />} />
+                     <Route path="/category/:categoryName" element={<ItemListContainer titulo="Los mejores estilos para ellos" parrafo="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, deserunt quisquam. Ut tempore vero, cupiditate dolorum enim repudiandae, illo voluptatibus nihil ab, obcaecati ipsa amet? Commodi tenetur aperiam modi dolorum."e />} />
+                     <Route path='/item/:idProd' element={<ItemDetailContainer />} />
+                     <Route path="/cart" element={<Cart />} />
+                     <Route path="/Checkout" element={<Checkout />} />
+                </Routes >
+              <Footer />
+            </BrowserRouter>
+    </CartProvider>
+    
+   
   );
 }
 
 export default App;
+
+    
+     
