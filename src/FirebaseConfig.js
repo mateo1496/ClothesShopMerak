1189c3dd0f673f4cb249e89ac452e40bb4f4ee11
mateo <mateo.b1496@gmail.com>
2022-11-11 10:58:44 -0300

    variables de entorno

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6qCghNWUcK9-kJHq0SOsc4vJTooZmxBQ",
-  authDomain: "clothesshopmerak.firebaseapp.com",
-  projectId: "clothesshopmerak",
-  storageBucket: "clothesshopmerak.appspot.com",
-  messagingSenderId: "524727702634",
-  appId: "1:524727702634:web:ab184de484bf272c6c6613"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID, 
 };
 
 
